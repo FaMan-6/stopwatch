78f46345d22afdfc9c5a3ae590b9a635a522931e
Faiz Abdurrahman <abdurr.dev@gmail.com>
2025-12-15 11:12:59 +0700

    fix vercel-build.sh line ending

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,15 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
-echo "Installing Flutter..."
-
-curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz | tar xJ
+echo "Downloading Flutter SDK..."
+curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz | tar xJ
 
 export PATH="$PATH:$PWD/flutter/bin"
 
-git config --global --add safe.directory /vercel/path0/flutter
-
-flutter config --no-analytics
-flutter doctor -v
-
+flutter --version
 flutter build web
